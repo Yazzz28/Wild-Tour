1b235631c2c2c8af0c81f287d5c78774f07231d7
Julien PRIVAT <144898664+Carcali@users.noreply.github.com>
2023-10-05 15:14:45 +0200

    Codes et assets modifiés ajoutés - 05/10/2023

---
+++ assets-julien/articles.php
@@ -0,0 +1,26 @@
+<?php
+
+// Articles récents
+
+$article = [
+    'Les articles récents' => [
+        [
+            'titre' => "Le début de la pêche dans l’Aube, Troyes",
+            'description' => 'Vestibulum consectetur nisi et tortor facilisis blandit. Ut elementum, dui ac tincidunt pretium, nunc eros iaculis mi, sed condimentum risus dui ac nibh.',
+            'image' => 'casa-del-arbol-ibiza-veranda-salon-exterieur.jpg',
+            'url' => 'fiche-daniel.html',
+        ],
+        [
+            'titre' => "Base sous-marine, Bordeaux",
+            'description' => 'Vestibulum consectetur nisi et tortor facilisis blandit. Ut elementum, dui ac tincidunt pretium, nunc eros iaculis mi, sed condimentum risus dui ac nibh.',
+            'image' => 'IMG_4122.jpg',
+            'url' => 'fiche-julien.php',
+        ],
+        [
+            'titre' => "Écoresponsabilité en entreprise, Libourne",
+            'description' => 'Vestibulum consectetur nisi et tortor facilisis blandit. Ut elementum, dui ac tincidunt pretium, nunc eros iaculis mi, sed condimentum risus dui ac nibh.',
+            'image' => 'IMG_4792.jpg',
+            'url' => 'fiche-stephv.html',
+        ],
+    ],
+];
