3f56bc6dca62a0ce366672e7eb0922838cf313ca
StyMey <stephanie2510@gmail.com>
2023-10-05 15:11:17 +0200

    commit stephm

---
+++ includes/navbar-menu/navbar.js
@@ -0,0 +1,19 @@
+<script>
+    var sidenav = document.getElementById("mySidenav");
+    var openBtn = document.getElementById("openBtn");
+    var closeBtn = document.getElementById("closeBtn");
+
+    openBtn.onclick = openNav;
+    closeBtn.onclick = closeNav;
+
+    /* Set the width of the side navigation to 250px */
+    function openNav() {
+        sidenav.classList.add("active");
+    }
+
+    /* Set the width of the side navigation to 0 */
+    function closeNav() {
+        sidenav.classList.remove("active");
+    }
+
+</script>
